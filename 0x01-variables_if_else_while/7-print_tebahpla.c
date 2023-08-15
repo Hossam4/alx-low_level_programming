01a8dcec0ec4aad56843606fb5fec99574042a4d
Hossam Gomah <hossamgomah4@gmail.com>
2023-08-14 19:29:19 -0700

    vbncvx

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: print all the alphapitical
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
