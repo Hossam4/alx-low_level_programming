e152b4761087e333cf60364c47bbca5414b265d3
FIRST_NAME LAST_NAME <hossamgomah4@gmail.com>
2023-08-29 10:44:43 -0700

    d

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all two two digits
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 98 ; i++)
+	{
+		j = i;
+		for (j = 0; j <= 99 ; j++)
+		{
+			if (j != i)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i + j != 35)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
