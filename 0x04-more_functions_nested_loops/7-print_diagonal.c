62bce8244cc306352e9f39542f4576296fb4878c
FIRST_NAME LAST_NAME <hossamgomah4@gmail.com>
2023-09-06 13:16:11 -0700

    g

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,16 @@ void print_diagonal(int n)
 	int row, space;
 
 	if (n <= 0)
-	{
+
 		_putchar('\n');
-	}
+
 	else
 	{
 		for (row = 1; row <= n; row++)
 		{
 			for (space = 1; space <= row; space++)
 				_putchar(' ');
-			_putchar(98);
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
