c5e97094c0bbcdd1bdd8c268f62f0eb352fccbe9
FIRST_NAME LAST_NAME <hossamgomah4@gmail.com>
2023-09-06 08:16:25 -0700

    g

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(d + 48);
+	}
+	_putchar('\n');
