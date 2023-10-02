2755f220a4e66a20c509e92af853b4ba595a4286
Hossam Gomah <hossamgomah4@gmail.com>
2023-10-02 15:16:15 -0700

    g

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <"stdio.h>
+
+/**
+* main - function that multiply
+* @argc: int
+* @argv: int
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int sum;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		sum = x * y;
+
+		printf(" % d\n", sum);
+	}
+	return (0);
+}
