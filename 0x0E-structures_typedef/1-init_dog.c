78e1ea6ed2e52d5ceba40f8b858d89c501d91964
Hossam Gomah <hossamgomah4@gmail.com>
2023-10-10 17:36:12 -0700

    g

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - init a dog's info
+ * @d: the dog to init
+ * @name: name of dog
+ * @age: age of it
+ * @owner: owner's name
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
