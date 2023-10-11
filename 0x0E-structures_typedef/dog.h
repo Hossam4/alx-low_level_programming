bd88c54c5cc4d6cc0cfb368b7fc7b1fd745dc3a7
Hossam Gomah <hossamgomah4@gmail.com>
2023-10-10 17:19:54 -0700

    g

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: name of dog
+ * @age: agee
+ * @owner: owner's name
+ *
+ * Description: long dog structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
