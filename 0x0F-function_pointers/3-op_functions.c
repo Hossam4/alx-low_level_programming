bc683e5840f4981d5e58e9c65002f267208359b2
Hossam Gomah <hossamgomah4@gmail.com>
2023-10-11 19:26:53 -0700

    l

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add strings
+ * @a: first int
+ * @b: sec int
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract strings
+ * @a: first int
+ * @b: sec int
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies ints
+ * @a: first int
+ * @b: sec int
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides integers
+ * @a: fisrst int
+ * @b: sec int
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - module integers
+ * @a: first int
+ * @b: sec int
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
