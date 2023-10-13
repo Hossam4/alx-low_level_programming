dcbfc9b28a164cd90e0a758bc0e97d3fb82f77c4
Hossam Gomah <hossamgomah4@gmail.com>
2023-10-12 19:22:23 -0700

    g

---
+++ 0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum arguments
+ * @n: args number
+ * @...: ints to sum
+ *
+ * Return: sum of ints
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int s = 0, i = n;
+	va_list ap;
+
+	if (!n)
+		return (0);
+
+	va_start(ap, n);
+	while (i--)
+		s += va_arg(ap, int);
+	va_end(ap);
+	return (s);
+}
